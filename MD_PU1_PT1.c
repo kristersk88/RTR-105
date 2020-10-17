07f68a2d8f253241986c3b344663d01320f1caaa
kristersk88 <71448944+kristersk88@users.noreply.github.com>
2020-10-17 20:25:16 +0300

    Add files via upload

---
+++ MD_PU1_PT1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+ float a, b, c = 0;
+    
+    printf("Ievadiet pirmo reizinātāju:\n");
+scanf("%f",&a);
+printf("Ievadiet otro reizinātāju:\n");
+scanf("%f",&b);
+c = a * b;
+ printf("Abu reizinājumu vērtība = %.f\n",c);
+
+    return 0;
+}
